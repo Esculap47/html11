06a4fb5552f525e710b4e5d94e1b62f3c1584d67
Esculap47 <ilyabezbakh395@gmail.com>
2021-01-28 18:45:52 +0300

    использовал 6 методов

--- script.js
+++ script.js
@@ -26,8 +26,14 @@ $(document).ready(function(){
     "hideEasing": "linear",
     "showMethod": "fadeIn",
     "hideMethod": "fadeOut"
-  }
+  };
 });
 
 
 
+$('a[href^="#"]').click(function(){ // #1
+  let anchor = $(this).attr('href');  // #2
+  $('html, body').animate({           // #3
+  scrollTop:  $(anchor).offset().top  // #4
+  }, 600);                            // #5
+  });
