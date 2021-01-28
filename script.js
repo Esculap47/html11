b0157e0e61dffb5ee3729de34f3e1e9b1084aad3
Esculap47 <ilyabezbakh395@gmail.com>
2021-01-28 18:38:55 +0300

    использовал плагин

--- script.js
+++ script.js
@@ -2,6 +2,32 @@ $(document).ready(function(){
   $( function() {
     $( "#accordion" ).accordion();
   } );
+   
 
-  
+
+
+  $(document).dblclick(function(){
+    toastr["info"]("информация", "вы нажали пкм два раза");
+  });
+
+   toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": true,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": true,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
 });
+
+
+
